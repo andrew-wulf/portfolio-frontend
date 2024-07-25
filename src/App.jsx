abd88e77f3a957eb75abf06139213e3f6570b2d5
awulf0 <andrewdwulf@gmail.com>
2024-07-25 12:32:43 -0500

    removed duplicate axios defaultURL set

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import axios from 'axios';
 
 function App() {
   axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://andrew-wulf-portfolio-api-486934fb1f0f.herokuapp.com/";
-
+  
   return (
     <div>
       <Routes>
